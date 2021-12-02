0096ea9d2c97991df55121de628ca4ca0053504b
wukai <wuk.jlu@outlook.com>
2021-12-02 17:53:45 +0800

    init repo

---
+++ src/main/java/com/example/miaoshabackend/MiaoshaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.miaoshabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoshaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiaoshaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miaoshabackend/MiaoshaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miaoshabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoshaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
